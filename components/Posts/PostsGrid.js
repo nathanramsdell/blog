030c9433d103fd9cb4703c21d122a2d1553b7ea9
Nathan Ramsdell <ramsdell.28@gmail.com>
2023-02-05 21:26:11 -0500

    Update pagination functino

--- components/Posts/PostsGrid.js
+++ components/Posts/PostsGrid.js
@@ -36,7 +36,7 @@ const PostsGrid = ({ posts }) => {
 export default PostsGrid
 
 export const getPagination = (page, size) => {
-    const limit = size ? +size : 3
+    const limit = +size 
     const from = page ? page * limit : 0
     const to = page ? from + size : size
     return { from, to };
